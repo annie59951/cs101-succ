6446d00813da755aa8e6157cc0da2618412ec146
annie59951 <annie59951@gmail.com>
2022-03-08 10:38:47 +0800

    add lotto.c_new

--- hw1/lotto.c
+++ hw1/lotto.c
@@ -2,62 +2,77 @@
 #include <stdlib.h>
 #include <time.h>
 FILE* fp;
-
+int exchange(a);
 void NowTime() {
+    
+    
     time_t curtime;
     time(&curtime);
     fprintf(fp,"%s",ctime(&curtime));
+    
 }
 
 void HowManyLotto(int *num) {
+    
     int a;
     printf("歡迎光臨長庚樂透彩購買機台\n");
-    printf("請問你要買幾組: ");
+    printf("請問你要買幾組:");
     scanf("%d",&a);
-    printf("已為您購買的 %d 組樂透組合輸入至 Lotto.txt\n",a);
+    printf("%d",a);
+    printf("已為您購買的%d組樂透組合輸入至 lotto.txt\n",a);
     *num = a;
     
 }
 
 int Num(int *num) {
-    int numbers[6];
-    int tmp = 0;
+    int a[6] = {0};
     srand(time(0));
     for(int i=0;i<*num;i++) {
-        fprintf(fp,"[%d]:",i+1);
-        
+        fprintf(fp,"[%d]",i+1);
         for(int j=0;j<6;j++) {
-            int n = rand()%69 + 1;
-            numbers[j] = n;  
-        } 
-        for(int z=5; z>=0; z--){
-            for(int x=0; x<z; x++){
-                if(numbers[x+1]<numbers[x]){
-                    tmp=numbers[x+1];
-                    numbers[x+1]=numbers[x];
-                    numbers[x]=tmp;
-                }
+            a[j] = rand()%69+1;
+            
+        }
+    exchange(a);    
+    for(int i=0;i<6;i++) {
+        fprintf(fp," %0*d",2,a[i]); 
+    }
+    
+    fprintf(fp," %0*d\n",2,rand()%10+1); 
+    }
+    
+}
+int exchange(int a[]) {
+    for(int i = 0;i<6;i++) {
+        for(int j=0;j<6;j++) {
+            if(a[j]>a[j+1]) {
+                int tmp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = tmp;
             }
         }
-    for(int q = 0; q<6; q++){
-        fprintf(fp, " %0*d",2,numbers[q]);
     }
-    fprintf(fp," %0*d\n",2,rand()%10 + 1); 
-    }   
 }
 
-int main(){
+            
+int main()
+{
+      
     fp = fopen("Lotto.txt","w+");
     int num = 0;
     HowManyLotto(&num);
     fprintf(fp,"========lotto649========\n");
     NowTime();
     Num(&num);
+    
     for(int i=0;i<5-num;i++) {
-        fprintf(fp,"[%d]: == == == == == == == ==\n", num + i + 1);
+        fprintf(fp,"[%d]",num+i+1);
+        fprintf(fp,"== == == == == == ==\n");
     }   
     
-    fprintf(fp,"======= csie@CGU =======\n");
+    fprintf(fp,"========================\n");
+    
+    
     fclose(fp);
     return 0;
 }
